4e150eed75915568c387120a92388397327a51b9
Hsakihara <sakiharu31415@yahoo.co.jp>
2020-05-13 13:31:56 +0900

    first commit

---
+++ server/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	fmt.Println("form: ", r.Form)
+	fmt.Println("path: ", r.URL.Path)
+	fmt.Println("scheme: ", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+
+	fmt.Fprintf(w, "Hello server!")
+}
+
+func main() {
+	fmt.Println("Server Start")
+
+	http.HandleFunc("/", sayhelloName)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
