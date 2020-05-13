85421a4252c71a63cd2a97414ef67f188fa3c7db
Hsakihara <sakiharu31415@yahoo.co.jp>
2020-05-13 04:51:59 +0000

    html/templateを導入

--- server/main.go
+++ server/main.go
@@ -2,19 +2,20 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"strings"
 )
 
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	req.ParseForm()
 
-	fmt.Println("form: ", r.Form)
-	fmt.Println("path: ", r.URL.Path)
-	fmt.Println("scheme: ", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
+	fmt.Println("form: ", req.Form)
+	fmt.Println("path: ", req.URL.Path)
+	fmt.Println("scheme: ", req.URL.Scheme)
+	fmt.Println(req.Form["url_long"])
+	for k, v := range req.Form {
 		fmt.Println("key:", k)
 		fmt.Println("val:", strings.Join(v, ""))
 	}
@@ -22,10 +23,29 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello server!")
 }
 
+type Page struct {
+	Title string
+	Count int
+}
+
+func viewHandler(w http.ResponseWriter, req *http.Request) {
+	tmpl, err := template.ParseFiles("templates/layout.html")
+	if err != nil {
+		log.Fatal("ParseFiles: ", err)
+		panic(err)
+	}
+
+	page := Page{"Hello World.", 1}
+	err = tmpl.Execute(w, page)
+	if err != nil {
+		log.Fatal("Execute: ", err)
+	}
+}
+
 func main() {
 	fmt.Println("Server Start")
 
-	http.HandleFunc("/", sayhelloName)
+	http.HandleFunc("/", viewHandler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
