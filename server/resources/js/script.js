838971a3748f23cff2b97c02749d671e4011f2bc
Hsakihara <sakiharu31415@yahoo.co.jp>
2020-05-16 11:28:30 +0000

    ローカルストレージの設定

--- server/resources/js/script.js
+++ server/resources/js/script.js
@@ -1,3 +1,17 @@
 $(function () {
-
+    if (localStorage.getItem("ranking") == null) {
+        var ranking = {
+            age: 0,
+            area: 99,
+            fromCharaId: [],
+            gender: 9,
+            idAnswered: false,
+            user: "",
+            votingHistory: [],
+            votingTodayHistory: []
+        };
+        localStorage.setItem("ranking", JSON.stringify(ranking));
+    };
+    
+    console.log(JSON.parse(localStorage.getItem("ranking")));
 });
