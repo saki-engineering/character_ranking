c0b5b77f83c88f193e05432caf7305b98d59716a
Hsakihara <sakiharu31415@yahoo.co.jp>
2020-05-16 12:34:00 +0000

    フォームのsubmit時に内容をローカルストレージに保存

--- server/resources/js/script.js
+++ server/resources/js/script.js
@@ -1,4 +1,8 @@
 $(function () {
+    //いつもローカルストレージの中身を表示
+    console.log(JSON.parse(localStorage.getItem("ranking")));
+
+    //いつもローカルストレージがクリアされていたらrankingを付与する
     if (localStorage.getItem("ranking") == null) {
         var ranking = {
             age: 0,
@@ -12,10 +16,19 @@ $(function () {
         };
         localStorage.setItem("ranking", JSON.stringify(ranking));
     };
-    
-    console.log(JSON.parse(localStorage.getItem("ranking")));
 
+    // characterページの投票ボタン
     $(".btn-vote").on('click', function() {
         console.log("投票ボタンだよ");
+    });
+
+    // フォームのsubmit時にローカルストレージに保存
+    $("#questionnaire").submit(function() {
+        var ranking = JSON.parse(localStorage.getItem("ranking"));
+        ranking.idAnswered = true;
+        ranking.age = Number($("#form-age").val());
+        ranking.gender = Number($("#form-gender").val());
+        ranking.area = Number($("#form-address").val());
+        localStorage.setItem("ranking", JSON.stringify(ranking));
     });
 });
