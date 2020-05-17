febc2df0cba94e18b74944f065e8b4429b4bf884
Hsakihara <sakiharu31415@yahoo.co.jp>
2020-05-17 05:20:50 +0000

    routerを別ディレクトリに分離

--- server/main.go
+++ server/main.go
@@ -5,29 +5,15 @@ import (
 	"log"
 	"net/http"
 
-	"./handlers"
 	"./middlewares"
-	"github.com/gorilla/mux"
+	"./routers"
 )
 
 func main() {
 	port := "8080"
 	fmt.Printf("Server Listening on port %s\n", port)
 
-	r := mux.NewRouter()
-	r.HandleFunc("/", handlers.ViewTopHandler)
-	r.HandleFunc("/about", handlers.ViewAboutHandler)
-	r.HandleFunc("/faq", handlers.ViewFaqHandler)
-
-	s1 := r.PathPrefix("/characters").Subrouter()
-	s1.HandleFunc("", handlers.CharacterHandler)
-	s1.HandleFunc("/{name}", handlers.CharacterDetailHandler)
-	s1.HandleFunc("/{name}/vote", handlers.CharacterVoteHandler).Methods("POST")
-	s1.HandleFunc("/{name}/voted", handlers.CharacterVotedHandler)
-
-	s2 := r.PathPrefix("/form").Subrouter()
-	s2.HandleFunc("", handlers.FormHandler)
-	s2.HandleFunc("/vote", handlers.FormVoteHandler).Methods("POST")
+	r := routers.CreateRouter()
 
 	fs := http.FileServer(http.Dir("./resources"))
 	r.PathPrefix("/resources/").Handler(http.StripPrefix("/resources/", fs))
---
+++ server/routers/mainrouter.go
@@ -0,0 +1,26 @@
+package routers
+
+import (
+	"../handlers"
+	"github.com/gorilla/mux"
+)
+
+// CreateRouter muxのルーターを作る
+func CreateRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", handlers.ViewTopHandler)
+	r.HandleFunc("/about", handlers.ViewAboutHandler)
+	r.HandleFunc("/faq", handlers.ViewFaqHandler)
+
+	s1 := r.PathPrefix("/characters").Subrouter()
+	s1.HandleFunc("", handlers.CharacterHandler)
+	s1.HandleFunc("/{name}", handlers.CharacterDetailHandler)
+	s1.HandleFunc("/{name}/vote", handlers.CharacterVoteHandler).Methods("POST")
+	s1.HandleFunc("/{name}/voted", handlers.CharacterVotedHandler)
+
+	s2 := r.PathPrefix("/form").Subrouter()
+	s2.HandleFunc("", handlers.FormHandler)
+	s2.HandleFunc("/vote", handlers.FormVoteHandler).Methods("POST")
+
+	return r
+}
