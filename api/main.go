cc0cabfcb6e6e1f017359b7da8f95d50b4e325d5
Hsakihara <sakiharu31415@yahoo.co.jp>
2020-05-19 22:43:27 +0900

    apiとmysqlを繋げた

--- api/main.go
+++ api/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
@@ -12,8 +14,20 @@ func main() {
 	port := "9090"
 	fmt.Printf("API Server Listening on port %s\n", port)
 
+	_, e := sql.Open("mysql", "root@/sampledb")
+	if e != nil {
+		log.Fatal("DB: ", e)
+	} else {
+		log.Println("Connected to mysql.")
+	}
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", RootHandler)
+
+	s1 := r.PathPrefix("/vote").Subrouter()
+	s1.HandleFunc("/", VoteRootHandler)
+	s1.HandleFunc("/{name}", CharaResultHandler).Methods("GET")
+	s1.HandleFunc("/{name}", VoteCharaHandler).Methods("POST")
 
 	err := http.ListenAndServe(":"+port, r)
 	if err != nil {
@@ -24,4 +38,25 @@ func main() {
 // RootHandler /のハンドラ
 func RootHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "Hello World")
+}
+
+// VoteRootHandler /vote/のハンドラ
+func VoteRootHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Vote root")
+}
+
+// CharaResultHandler /vote/{name}のGETハンドラ
+func CharaResultHandler(w http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	fmt.Fprintf(w, vars["name"])
+}
+
+// VoteCharaHandler /vote/{name}のPOSTハンドラ
+// テストするためには $curl -X POST -d "character=cinamon" localhost:9090/vote/name
+func VoteCharaHandler(w http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	fmt.Fprintf(w, vars["name"])
+
+	req.ParseForm()
+	fmt.Println("form: ", req.Form)
 }
