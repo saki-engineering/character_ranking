64bc46951d940f0aeee4c786cae4858c61e8b1ea
Hsakihara <sakiharu31415@yahoo.co.jp>
2020-05-21 21:44:08 +0900

    form経由でもapiを叩くようにした

--- server/handlers/formhandler.go
+++ server/handlers/formhandler.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 // FormHandler フォームを表示
@@ -21,9 +23,27 @@ func FormHandler(w http.ResponseWriter, req *http.Request) {
 
 // FormVoteHandler アンケートフォームから投票した時のハンドラ
 func FormVoteHandler(w http.ResponseWriter, req *http.Request) {
-	//ここにuser登録処理と、投票処理が入る
-
 	req.ParseForm()
+	//ここにuser登録処理
+
+	//投票処理が入る
+	client := new(http.Client)
+
+	u := &url.URL{}
+	u.Scheme = "http"
+	u.Host = "vote_api:9090"
+	u.Path = "/vote/"
+	uStr := u.String()
+
+	values := url.Values{}
+	values.Add("character", req.Form.Get("character"))
+
+	_, err := client.Post(uStr, "application/x-www-form-urlencoded", strings.NewReader(values.Encode()))
+	if err != nil {
+		log.Println("client post err: ", err)
+		return
+	}
+
 	url := "/characters/" + req.Form["character"][0] + "/voted"
 	http.Redirect(w, req, url, 302)
 }
