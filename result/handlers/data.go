8fd200cf297efc1c7da26fb630b54e337a3f14f2
Hsakihara <sakiharu31415@yahoo.co.jp>
2020-05-27 19:47:16 +0900

    Page構造体に関することを別ファイルに移動

---
+++ result/handlers/data.go
@@ -0,0 +1,33 @@
+package handlers
+
+//Page ... htmlに渡す値をまとめた構造体
+type Page struct {
+	Title     string
+	UserID    string
+	LogIn     bool
+	Admin     bool
+	Character []VoteResult
+}
+
+// VoteResult キャラクターごとの得票数をまとめた構造体
+type VoteResult struct {
+	ID   int
+	Name string
+	Vote int
+}
+
+var charas = []VoteResult{
+	VoteResult{1, "cinnamon", 0},
+	VoteResult{2, "cappuccino", 0},
+	VoteResult{3, "mocha", 0},
+	VoteResult{4, "chiffon", 0},
+	VoteResult{5, "espresso", 0},
+	VoteResult{6, "milk", 0},
+	VoteResult{7, "azuki", 0},
+	VoteResult{8, "coco", 0},
+	VoteResult{9, "nuts", 0},
+	VoteResult{10, "poron", 0},
+	VoteResult{11, "corne", 0},
+	VoteResult{12, "berry", 0},
+	VoteResult{13, "cherry", 0},
+}
--- result/handlers/handler.go
+++ result/handlers/handler.go
@@ -11,36 +11,6 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-//Page ... htmlに渡す値をまとめた構造体
-type Page struct {
-	Title     string
-	UserID    string
-	LogIn     bool
-	Character []VoteResult
-}
-
-// VoteResult キャラクターごとの得票数をまとめた構造体
-type VoteResult struct {
-	Name string
-	Vote int
-}
-
-var charas = []VoteResult{
-	VoteResult{"cinnamon", 0},
-	VoteResult{"cappuccino", 0},
-	VoteResult{"mocha", 0},
-	VoteResult{"chiffon", 0},
-	VoteResult{"espresso", 0},
-	VoteResult{"milk", 0},
-	VoteResult{"azuki", 0},
-	VoteResult{"coco", 0},
-	VoteResult{"nuts", 0},
-	VoteResult{"poron", 0},
-	VoteResult{"corne", 0},
-	VoteResult{"berry", 0},
-	VoteResult{"cherry", 0},
-}
-
 // RootHandler /のハンドラ
 func RootHandler(w http.ResponseWriter, req *http.Request) {
 	tmpl, err := loadTemplate("index")
